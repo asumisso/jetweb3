4b9e77231f19e92a87617045b70a7de989679dba
asumisso <karaage.imps@gmail.com>
2016-06-15 15:22:46 +0900

    temp

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+
+  jQuery(function() {
+    var nav = jQuery('#sub-nav');　//固定したいDivのIDを入力
+
+    // メニューのtop座標を取得する
+    var offsetTop = nav.offset().top;
+
+    var floatMenu = function() {
+      // スクロール位置がメニューのtop座標を超えたら固定にする
+      if (jQuery(window).scrollTop() > offsetTop) {
+        nav.addClass('fixed');
+      } else {
+        nav.removeClass('fixed');
+      }
+    }
+    jQuery(window).scroll(floatMenu);
+    jQuery('body').bind('touchmove', floatMenu);
+  });
